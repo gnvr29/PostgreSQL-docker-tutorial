a1fd6ad7b4f76ee35b4f3778460012e49244f29f
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-07-27 14:29:49 -0300

    feat: basic commands for the tutorial

---
+++ commands.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50) UNIQUE NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
+INSERT INTO users (username, email) VALUES ('john_doe', 'john.doe@example.com');
